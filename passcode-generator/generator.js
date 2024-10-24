28d01aa9160d194ce2bd6031a105de777393acbc
houdabouta <houda@cloudi-fi.com>
2024-10-24 14:53:51 +0200

    Update generator.js

--- passcode-generator/generator.js
+++ passcode-generator/generator.js
@@ -1,18 +1,18 @@
-
 const crypto = require('crypto');
 
+// Function to generate a weekly passcode
 function generatePasscode() {
     const specialCharacters = '!@#$%^&*';
     const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     const digits = '0123456789';
-    return (
-        letters.charAt(crypto.randomInt(letters.length)) +
-        letters.charAt(crypto.randomInt(letters.length)).toLowerCase() +
-        letters.charAt(crypto.randomInt(letters.length)).toUpperCase() +
-        digits.charAt(crypto.randomInt(digits.length)) +
+
+    return [
+        letters.charAt(crypto.randomInt(letters.length)),
+        letters.charAt(crypto.randomInt(letters.length)).toLowerCase(),
+        letters.charAt(crypto.randomInt(letters.length)).toUpperCase(),
+        digits.charAt(crypto.randomInt(digits.length)),
         specialCharacters.charAt(crypto.randomInt(specialCharacters.length))
-    );
+    ].join('');
 }
 
 module.exports = generatePasscode;
-    
